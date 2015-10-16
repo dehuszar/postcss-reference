2aa59db163c3dfb10fa238561188d1f3560927fa
Samuel Allen <dehuszar@gmail.com>
2015-10-16 08:53:16 -0500

    tuning test scripts.  woes of copy/paste

--- dist/test/test.js
+++ dist/test/test.js
@@ -1,13 +1,25 @@
 'use strict';
 
-import test from 'tape';
-import postcss from 'postcss';
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _tape = require('tape');
+
+var _tape2 = _interopRequireDefault(_tape);
+
+var _postcss = require('postcss');
+
+var _postcss2 = _interopRequireDefault(_postcss);
+
+var _indexJs = require('../../index.js');
+
+var _indexJs2 = _interopRequireDefault(_indexJs);
+
+var _packageJson = require('../../package.json');
+
 var atImport = require("postcss-import");
 var nested = require("postcss-nested");
-import plugin from '../../index.js';
-import {name} from '../../package.json';
 
-let tests = [{
+var tests = [{
     message: 'should match requested selector',
     fixture: '@reference{ header { color: blue; display: block; } } header { @references(header); display: block; margin: 1em; }',
     expected: 'header { color: blue; display: block; margin: 1em; }'
@@ -29,21 +41,21 @@ let tests = [{
     expected: 'header { color: blue; display: block; margin: 1em; }\nheader aside { width: 25%; }'
 }];
 
-function process (css, options) {
-    return postcss().use(atImport()).use(nested()).use(plugin(options)).process(css).css;
+function process(css, options) {
+    return (0, _postcss2['default'])().use(atImport()).use(nested()).use((0, _indexJs2['default'])(options)).process(css).css;
 }
 
-test(name, t => {
+(0, _tape2['default'])(_packageJson.name, function (t) {
     t.plan(tests.length);
 
-    tests.forEach(test => {
-        let options = test.options || {};
+    tests.forEach(function (test) {
+        var options = test.options || {};
         t.equal(process(test.fixture, options), test.expected, test.message);
     });
 });
 
-test('should use the postcss plugin api', t => {
+(0, _tape2['default'])('should use the postcss plugin api', function (t) {
     t.plan(2);
-    t.ok(plugin().postcssVersion, 'should be able to access version');
-    t.equal(plugin().postcssPlugin, name, 'should be able to access name');
+    t.ok((0, _indexJs2['default'])().postcssVersion, 'should be able to access version');
+    t.equal((0, _indexJs2['default'])().postcssPlugin, _packageJson.name, 'should be able to access name');
 });
